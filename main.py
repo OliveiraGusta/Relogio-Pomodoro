e3430959c38630f9e6c49deda8842a8eff8accd0
OliveiraGusta <gustavo.oliver456@gmail.com>
2023-12-20 01:29:16 -0300

    Feat<Fixar>: Adicionado um botao de fixar a janela

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class RelogioPomodoro:
         self.style = Style(theme="minty")
         self.style.theme_use()
 
-
         self.tempo_label = tk.Label(self.root, text="25:00", font=("TkDefaultFont", 60))
         self.tempo_label.pack(pady=20)
 
@@ -32,6 +31,11 @@ class RelogioPomodoro:
 
         self.menos_5_min_button = ttk.Button(button_frame, text="-5 Min", style="secondary",  command=self.subtrair_tempo)
         self.menos_5_min_button.pack(side=tk.LEFT, padx=5)
+
+        self.fixar_janela_var = tk.BooleanVar()
+        self.fixar_janela_var.set(False)
+        self.fixar_janela_button = ttk.Checkbutton(button_frame, text="Fixar", style="info", variable=self.fixar_janela_var, command=self.fixar_janela)
+        self.fixar_janela_button.pack(side=tk.LEFT, padx=5)
 
         self.tempo_tarefa, self.tempo_pausado = TEMPO_TAREFA, PAUSA_CURTA
         self.tarefa_esta_ativa, self.pomodoros_completo, self.esta_rodando = True, 0, False
@@ -83,31 +87,33 @@ class RelogioPomodoro:
         if not self.esta_rodando:
             if not self.tarefa_esta_ativa:
                 self.tempo_pausado += 5 * 60
-                self.atualizar_label()
+                self.atualizar_tempo()
             else:
                 self.tempo_tarefa += 5 * 60
-                self.atualizar_label()
+                self.atualizar_tempo()
         
-
     def subtrair_tempo(self):
         if not self.esta_rodando:
             if not self.tarefa_esta_ativa:
                 self.tempo_pausado -= 5 * 60
-                self.atualizar_label()
+                self.atualizar_tempo()
             else:
                 self.tempo_tarefa -= 5 * 60
                 if self.tempo_tarefa < 0:
                     self.tempo_tarefa = 0
-                self.atualizar_label()
+                self.atualizar_tempo()
 
-        
-    
-    def atualizar_label(self):
+    def atualizar_tempo(self):
         if not self.tarefa_esta_ativa:
             minutos, segundos = divmod(self.tempo_pausado, 60)
             self.tempo_label.config(text="{:02d}:{:02d}".format(minutos, segundos))
         else:
             minutos, segundos = divmod(self.tempo_tarefa, 60)
             self.tempo_label.config(text="{:02d}:{:02d}".format(minutos, segundos))
+
+    def fixar_janela(self):
+        estado_fixo = self.fixar_janela_var.get()
+        self.root.attributes('-topmost', estado_fixo)
+        
      
 RelogioPomodoro()
